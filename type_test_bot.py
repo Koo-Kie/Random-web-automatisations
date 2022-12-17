d863477fcde9dff07a1ac19bd750e42ee8c8e277
Kookie <94642930+Koo-Kie@users.noreply.github.com>
2022-12-17 22:09:41 +0100

    Update type_test_bot.py

--- type_test_bot.py
+++ type_test_bot.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import keyboard, time 
+import keyboard
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.livechat.com/typing-speed-test/#/')
@@ -13,5 +13,5 @@ keyboard.wait('enter')
 while not driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/div[1]/div/span/div[1]/div/div[1]/div/div[1]').text == '0':
     content = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/div[1]/div/span/div[2]/span/div/div[2]/div[2]/span[1]')
     keyboard.write(f'{content.text} ')
-    time.sleep(.1)
+    sleep(.1)
 input("Press enter")
